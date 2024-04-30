ad1a627b1bd09e2e0c7fa12971312a4085438c95
tylerewing64 <76537101+tylerewing64@users.noreply.github.com>
2024-04-29 23:27:47 -0400

    Added login screen

--- annon_client/react-client/src/App.js
+++ annon_client/react-client/src/App.js
@@ -2,9 +2,19 @@ import './/css/App.css';
 
 function App() {
   return (
+    <body>
     <div className = "login-container"> 
-    <p>test</p>
+    <div className = "login-wrapper">
+      <h1>AnnonChat.</h1> 
+      <p>Username:</p>
+      <input type = "text" id = "username-input"/>
+      <p>Password:</p>
+      <input type = "password" id = "password-input"/>
+      <button type ="button" id ="submit">Login🔒</button>
+      <a href ="#">Forgot password?</a>
     </div>
+    </div>
+    </body>
   );
 }
 
